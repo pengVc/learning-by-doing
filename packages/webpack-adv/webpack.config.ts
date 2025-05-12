4460ceed5cf5c56786aab5601bc871835152a554
pengVc <penjuntan@gmail.com>
2025-05-12 15:02:01 +0800

    feat(webpack-adv): 添加 HooksPractisePlugin 插件- 新增 HooksPractisePlugin 插件，实现以下功能：  - 在 emit 钩子中删除 output 目录下的所有文件   - 在 emit钩子中生成自定义的 readme 文件 - 更新 webpack 配置，添加 HooksPractisePlugin插件

--- packages/webpack-adv/webpack.config.ts
+++ packages/webpack-adv/webpack.config.ts
@@ -1,5 +1,6 @@
 import type { Configuration } from 'webpack'
 import HooksLifecyclePlugin from '@lbd/webpack-adv/plugins/hooks-lifecycle-plugin'
+import HooksPractisePlugin from '@lbd/webpack-adv/plugins/hooks-practise-plugin'
 
 
 const config: Configuration = {
@@ -8,7 +9,7 @@ const config: Configuration = {
     path: __dirname + '/dist',
     filename: 'ts-[name].[chunkhash].js',
   },
-  plugins: [new HooksLifecyclePlugin()],
+  plugins: [new HooksLifecyclePlugin(), new HooksPractisePlugin()],
 }
 
 export default config
