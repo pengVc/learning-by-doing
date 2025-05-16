76e7e5a96b9a6685e056c0c877f51dd5f94f221c
pengVc <penjuntan@gmail.com>
2025-05-16 12:36:55 +0800

    feat(demo): 添加 eval 页面并实现相关功能
    - 在 eval 页面中添加了多个函数来演示 eval 的使用方法和行为
    - 新增了 index.html 页面作为入口- 更新了 vite.config.ts 文件以支持多页面应用
    - 添加了 .gitignore 文件以忽略不必要的文件和目录

--- packages/demo/src/pages/eval/main.ts
+++ packages/demo/src/pages/eval/main.ts
@@ -1,5 +1,92 @@
-const main = () => {
-  console.log('hello eval')
+// @ts-nocheck
+
+window.x = 'global'
+
+/**
+ * 直接调用 eval
+ */
+function directEval() {
+  'use strict'
+  var x = 'local'
+
+  // local
+  // 可以解析当前(调用 eval 的)作用域的变量
+  console.log('directEval: ', eval('x')) // => 'local'
+}
+directEval()
+
+/**
+ * 间接调用 eval
+ */
+function indirectEval() {
+  'use strict'
+  var x = 'local'
+
+  // 非直接调用 eval 的几种方式
+  console.log('indirectEval: ', eval.call(null, 'x')) // => 'global'
+  console.log('indirectEval: ', window.eval('x')) // => 'global'
+  console.log('indirectEval: ', (1, eval)('x')) // => 'global'
+  var aliasEval = eval
+  console.log('indirectEval: ', aliasEval('x')) // => 'global'
+
+  aliasEval('var _foo = 3')
+  console.log('indirectEval: ', window._foo) // => 3
+
+  var obj = { eval: eval }
+  console.log('indirectEval: ', obj.eval('x')) // => global
+}
+indirectEval()
+
+function newFnWithDeclare() {
+  var foo
+  var fn = new Function('var foo = 1')
+
+  fn()
+  // `undefined` 因为 Function 构造函数创造的变量 foo 并不在全局范围内!
+  console.log('new Function (with declare): ', typeof foo) // => undefined
+
+  if (typeof window !== 'undefined') {
+    console.log('new Function (with declare): ', window.foo) // => undefined
+  } else if (typeof global !== 'undefined') {
+    console.log('new Function (with declare): ', global.foo) // => undefined
+  }
+}
+newFnWithDeclare()
+
+function newFnWithoutDeclare() {
+  var foo
+  var fn = new Function('foo = 1024')
+
+  fn()
+
+  console.log('new Function (without declare): ', foo) // => undefined
+  if (typeof window !== 'undefined') {
+    console.log('new Function (without declare): ', window.foo) // => 1024
+  } else if (typeof global !== 'undefined') {
+    console.log('new Function (without declare): ', global.foo) // => 1024
+  }
 }
 
-main()
+newFnWithoutDeclare()
+
+function thisOfStrictFunc() {
+  var sl = new Function('return this')
+  console.log('unStrict+unStrict this:', sl()) // => window or global
+
+  var st = new Function('"use strict"; return this')
+  // 严格模式下, 禁止 this 关键字执行全局对象!
+  console.log('unStrict+strict this:', st()) // => undefined
+}
+thisOfStrictFunc()
+
+function thisOfUnStrictFunc() {
+  'use strict'
+  var sl = new Function('return this')
+  // 不受当前作用域影响
+  console.log('strict+unStrict this:', sl()) // => window or global
+
+  var st = new Function('"use strict"; return this')
+  // 严格模式下, 禁止 this 关键字执行全局对象!
+  console.log('strict+strict this:', st()) //  => undefined
+}
+thisOfUnStrictFunc()
--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -5,23 +5,38 @@ import path from 'path'
 const mpaPrefix = 'src/pages'
 
 // 获取所有页面入口
-const pages = fs.readdirSync(path.resolve(__dirname, mpaPrefix))
+const pages = fs
+  .readdirSync(path.resolve(__dirname, mpaPrefix), {
+    withFileTypes: true,
+  })
+  .filter((dirent) => dirent.isDirectory())
 
 export default defineConfig((config) => {
+  const input = pages.reduce<Record<string, string>>(
+    (acc, page) => {
+      const { name } = page
+      acc[name] = path.resolve(__dirname, `${mpaPrefix}/${name}/index.html`)
+      return acc
+    },
+    {
+      index: path.resolve(__dirname, `${mpaPrefix}/index.html`),
+    },
+  )
+
+  console.log({ input })
+
   return {
     plugins: [],
     build: {
       rollupOptions: {
-        input: pages.reduce<Record<string, string>>((acc, page) => {
-          acc[page] = path.resolve(__dirname, `${mpaPrefix}/${page}/index.html`)
-          return acc
-        }, {}),
+        input,
       },
+      // 相对于项目根目录 (root)
       outDir: path.resolve(__dirname, 'dist'),
     },
-    base: './',
     // 指定项目根目录，避免了访问 MPA ，需要带上 src/pages
     root: mpaPrefix,
+    base: './',
     server: {
       port: 3000,
     },
