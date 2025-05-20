aa5d7d5e562d51d8f48e336f6137f48514cdbc0f
pengVc <penjuntan@gmail.com>
2025-05-20 15:49:13 +0800

    feat(webpack-adv): 添加删除目录插件并优化现有插件
    - 新增 DeleteDirPlugin插件，用于删除指定目录- 重构 HooksPractisePlugin，使用 fs/promises 替代 fs
    - 新增 getErrorMsg工具函数，用于获取错误信息
    - 更新 webpack 配置，添加 DeleteDirPlugin

---
+++ packages/webpack-adv/plugins/delete-dir.ts
@@ -0,0 +1,68 @@
+import { Compiler, WebpackPluginInstance } from 'webpack'
+import * as fs from 'node:fs/promises'
+import getErrorMsg from '../utils/getErrorMsg'
+import path from 'node:path'
+
+type DeleteDirPluginOptions = {
+  dir: string
+}
+
+class DeleteDirPlugin implements WebpackPluginInstance {
+  private options: DeleteDirPluginOptions
+
+  constructor(options: DeleteDirPluginOptions) {
+    this.options = options
+  }
+
+  apply(compiler: Compiler) {
+    const { dir } = this.options
+
+    // node version >= 14.14
+    compiler.hooks.emit.tapPromise('DeleteDirPlugin', async (compilation) => {
+      const outputDir = compiler.options.output.path
+
+      if (!outputDir) {
+        console.log('Empty outputDir')
+        return
+      }
+
+      try {
+        await fs.rm(dir, { recursive: true, force: true })
+      } catch (error) {
+        compilation.errors.push(
+          new Error(`[DeleteDirPlugin] Failed to delete directory: ${dir}. ${getErrorMsg(error)}`),
+        )
+      }
+    })
+
+    // node version < 14
+    0 &&
+      compiler.hooks.emit.tapAsync('HooksPractisePlugin', async (compilation, cb) => {
+        const outputDir = compiler.options.output.path
+
+        if (!outputDir) {
+          console.log('Empty outputDir')
+          return
+        }
+
+        // 遍历目录中的每个文件
+        const files = await fs.readdir(outputDir, {
+          recursive: true,
+          withFileTypes: true,
+        })
+
+        await Promise.all(
+          files
+            .filter((file) => file.isFile())
+            .map((file) => {
+              // 删除文件
+              return fs.unlink(path.join(file.parentPath, file.name))
+            }),
+        )
+
+        cb()
+      })
+  }
+}
+
+export default DeleteDirPlugin
