41dfb6f55468d76b45559d06b795ba998b0c2d41
pengVc <penjuntan@gmail.com>
2025-05-20 16:05:47 +0800

    refactor(ssr-adv):重构项目并添加 TailwindCSS- 删除原有 Vite + TypeScript 示例代码 - 新增 TailwindCSS 配置和样式 - 创建新的 App 组件 - 更新项目结构和配置文件 - 添加 React 和 TailwindCSS 依赖 - 统一 typescript 为跟目录依赖

---
+++ packages/ssr-adv/src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import App from './components/App'
+import './style.css'
+
+const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement)
+
+root.render(<App />)
